8df611c40f40d8eea7679290f0be42ae2c506d19
Cami Sebellin <csebellin@fi.uba.ar>
2022-11-24 13:29:29 -0300

    agrego manejo de horas en parte de horas

--- src/main/java/com/recursos/model/ParteDeHoras.java
+++ src/main/java/com/recursos/model/ParteDeHoras.java
@@ -1,6 +1,7 @@
 package com.recursos.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class ParteDeHoras {
@@ -9,10 +10,16 @@ public class ParteDeHoras {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long parteDeHorasID;
 
+    private Date fecha;
+
     public ParteDeHoras() {
     }
 
     public Long getParteDeHorasID() {return parteDeHorasID; }
 
+    public Long getFecha() {return fecha.getTime(); }
+
     public void setParteDeHorasID(Long parteDeHorasID) { this.parteDeHorasID = parteDeHorasID; }
+
+    public void setFecha(Date fecha) { this.fecha = fecha; }
 }
