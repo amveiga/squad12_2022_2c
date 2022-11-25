7a2cd8b6dc6aefd799c8ae654f2e5d6c14fcb390
Cami Sebellin <csebellin@fi.uba.ar>
2022-11-25 17:31:54 -0300

    Fix: Exceptions arregladas

--- src/main/java/com/recursos/exceptions/LegajoNoEncontradoException.java
+++ src/main/java/com/recursos/exceptions/LegajoNoEncontradoException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class InsufficientFundsException extends RuntimeException {
+public class LegajoNoEncontradoException extends RuntimeException {
 
-    public InsufficientFundsException(String message) {
+    public LegajoNoEncontradoException(String message) {
         super(message);
     }
 }
--- src/main/java/com/recursos/exceptions/ParteDeHorasNoEncontradoException.java
+++ src/main/java/com/recursos/exceptions/ParteDeHorasNoEncontradoException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class DepositNegativeSumException extends RuntimeException {
+public class ParteDeHorasNoEncontradoException extends RuntimeException {
 
-    public DepositNegativeSumException(String message) {
+    public ParteDeHorasNoEncontradoException(String message) {
         super(message);
     }
 }
